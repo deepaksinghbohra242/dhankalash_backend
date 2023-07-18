2f6897af8e84ca20e76a7368de44bd7495b5a7b1
Deepak Singh Bohra <deepaksinghbohra242@gmail.com>
2023-07-18 14:25:39 +0530

    done

---
+++ src/main/java/com/dhankalash/dhankalash/DhankalashApplication.java
@@ -0,0 +1,13 @@
+package com.dhankalash.dhankalash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhankalashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhankalashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhankalash/dhankalash/DhankalashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhankalash.dhankalash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhankalashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
